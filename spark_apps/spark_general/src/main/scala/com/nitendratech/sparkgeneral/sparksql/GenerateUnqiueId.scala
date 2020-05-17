a2025147b15baeb01ececfc4e158fb12fdacafbc
nitendragautam <nitendra.gautam12@gmail.com>
2020-05-17 18:04:42 -0500

    Adding new spark code on Generating UUID in Scala

---
+++ spark_apps/spark_general/src/main/scala/com/nitendratech/sparkgeneral/sparksql/GenerateUnqiueId.scala
@@ -0,0 +1,13 @@
+package com.nitendratech.sparkgeneral.sparksql
+
+/**
+ * Created by @author nitendratech on 5/16/20
+ */
+object GenerateUnqiueId {
+
+  def main(args: Array[String]): Unit = {
+  }
+
+
+
+}
